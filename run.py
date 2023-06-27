b6a232946fd8259869ada1555f458ef928a387b9
Amir Salimi <asalimi.94@gmail.com>
2023-06-27 16:34:34 +0000

    define board

--- run.py
+++ run.py
@@ -50,3 +50,21 @@ class Ship:
         self.coords = []
         # Number of hits taken by the ship
         self.hits = 0
+
+        def is_sunk(self):
+        # Return True if the ship is sunk, False otherwise(return 0 for False and a number for True)
+        return self.hits == self.size
+
+
+# Define a class for the board
+class Board:
+    def __init__(self, size):
+        # Size of the board
+        self.size = size
+        # Grid of cells
+        # changing cell for diffrent cell character -- change line
+        self.grid = [["." for _ in range(size)] for _ in range(size)]
+        # List of ships on the board
+        self.ships = []
+        # Number of shots fired on the board
+        self.shots = 0
