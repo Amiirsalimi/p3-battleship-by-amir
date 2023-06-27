9afb0ece4bdedc456289d035d093e19abc78e814
Amir Salimi <asalimi.94@gmail.com>
2023-06-27 15:13:57 +0000

    add loop for animation

--- run.py
+++ run.py
@@ -1,3 +1,40 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+import time
+
+
+def animation():
+    animation = [
+        "[        ]",
+        "[=       ]",
+        "[===     ]",
+        "[====    ]",
+        "[=====   ]",
+        "[======  ]",
+        "[======= ]",
+        "[========]",
+        "[ =======]",
+        "[  ======]",
+        "[   =====]",
+        "[    ====]",
+        "[     ===]",
+        "[      ==]",
+        "[       =]",
+        "[        ]",
+        "[        ]",
+    ]
+
+        notcomplete = True
+    i = 0
+    wait_time = random.randint(1, 4)
+    timeout = time.time() + wait_time
+    while notcomplete:
+        print(animation[i % len(animation)], end="\r")
+        time.sleep(0.1)
+        i += 1
+        if time.time() > timeout:
+            break
+    print()
